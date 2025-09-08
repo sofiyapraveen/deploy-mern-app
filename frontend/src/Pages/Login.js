523f3c8abb5afc4fae205d14bd6bdaad6c841895
Sofiya Praveen <168554260+sofiyapraveen@users.noreply.github.com>
2025-09-08 20:35:11 +0530

    Update Login.js

--- frontend/src/Pages/Login.js
+++ frontend/src/Pages/Login.js
@@ -25,7 +25,7 @@ const Login = () => {
     }
 
     try {
-      const url = "http://localhost:8080/auth/login";
+      const url = "https://deploymernappapi.vercel.app/auth/login";
       const response = await fetch(url, {
         method: "POST",
         headers: { 'Content-Type': 'application/json' },
